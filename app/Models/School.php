35f78aa464d7fc19b06c19da7d0388011120d427
Darwin Salinas <salinash2000@gmail.com>
2023-01-17 17:55:20 -0600

    Add model and migration to schools table

--- app/Models/School.php
+++ app/Models/School.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'hs_id',
+        'director_id',
+        'mec_id',
+        'country_id',
+        'region_id',
+        'city_id',
+        'name',
+        'postal',
+        'phone',
+        'password',
+        'email',
+        'email2',
+        'website',
+        'fax',
+        'address',
+        'address_short',
+        'latitude',
+        'longitude',
+        'plan_preference',
+        'leads',
+        'business_status',
+        'google_user_ratings_total',
+        'google_rating',
+        'revisor',
+        'active'
+    ];
 }
--- database/migrations/2023_01_17_215736_create_schools_table.php
+++ database/migrations/2023_01_17_215736_create_schools_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,31 @@ return new class extends Migration
     {
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('hs_id');
+            $table->unsignedBigInteger('director_id');
+            $table->unsignedBigInteger('mec_id');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('region_id');
+            $table->unsignedBigInteger('city_id');
+            $table->string('name', 100);
+            $table->string('postal', 10);
+            $table->string('phone', 15);
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->string('email2')->unique();
+            $table->string('website');
+            $table->string('fax')->nullable();
+            $table->string('address');
+            $table->string('address_short', 100);
+            $table->string('latitude', 30);
+            $table->string('longitude', 30);
+            $table->string('plan_preference', 30);
+            $table->tinyInteger('leads');
+            $table->string('business_status', 60);
+            $table->tinyInteger('google_user_ratings_total');
+            $table->string('google_rating', 3);
+            $table->string('revisor', 100);
+            $table->tinyInteger('active');
             $table->timestamps();
         });
     }
